48bfe9cabe1d1658e9e955f45fff57701721d791
unknown <a.adhikari6960@gmail.com>
2020-12-02 12:59:19 +0545

    Iterator and and two pointers have been setup.

--- project3.s
+++ project3.s
@@ -7,3 +7,7 @@
             la $a0, reply
             li $a1, 1001
             syscall
+					#Loading the input to a regsiter for conversion to decimal
+            la $s0, reply
+            li $s1, 0 			#start pointer to make sure that we collect only 4 characters and a comma.
+            li $s2, 0			#end pointer to make sure what characters we have collected and read.
