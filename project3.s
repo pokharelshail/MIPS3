d38584b37c685a543763ba4ed5a7ac5133dccd0c
unknown <a.adhikari6960@gmail.com>
2020-12-02 13:33:19 +0545

    Exiting the loop if the first character is space, empty or new line, If they are the characters listed before, the loop will end and take us to end_of_substring label which will then print the values inside our main program.

--- project3.s
+++ project3.s
@@ -16,3 +16,7 @@
 	substring:
 	    add $t1, $s0, $s2 	        #iterator taking the pointers sum
             lb $t2, 0($t1) 		#loading the current character
+
+	    beq $t2, 0, end_of_substring #a few criteron to exit the loop while iterating through the substrings
+            beq $t2, 10, end_of_substring
+            beq $t2, 44, end_of_substring
